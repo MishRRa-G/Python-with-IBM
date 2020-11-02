66adaca55a0fab01ccbfdf5e9724d248830bb71d
Ayush Mishra <46668660+MishraBlaze84@users.noreply.github.com>
2020-11-02 11:51:57 +0530

    Create PythonPatterns.py

---
+++ PythonPatterns.py
@@ -0,0 +1,6 @@
+#This program prints the different patters, I will start from simple patterns.
+for i in range(1,10,2):#This will start a loop of row with 0 and add 2 each time until 10
+    for me in range(i+1):# this loop will start from 0 to i+1 and run Completely for each Value of I.
+        print(me,end='')
+    print('\n')
+    
